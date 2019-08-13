2f7c600184bd4426a584d9372993afa93d12dae6
acdc@plaz.com.ua <acdc@plaz.com.ua>
2019-08-13 15:47:37 +0300

    ver.3

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -1,36 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
+
+
 namespace Test2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<Double[]> st = new List<Double[]>();
-            try
+            public List<string> GetAllFiles(string sDirt);
             {
-                //for (string. i = 0; string. i < 12; string. i++)
-                
-                foreach (string line in System.IO.File.ReadLines($@"C:\\Users\\USER\\source\\repos\\Test2\\Test2\\Data\\Compartments\\S1.csv"))
-                    st.Add(line.Trim().Replace('.', ',').Split(';').Select(double.Parse).ToArray());
+                List<string> st = new List<string>();
+
+                //  List<Double[]> st = new List<Double[]>();
+                try
+                {
+                    foreach (string file in Directory.GetFiles(sDirt))
+                    {
+                        st.Add(file);
+                    }
+                    foreach (string fl in Directory.GetDirectories(sDirt))
+                    {
+                        st.AddRange(GetAllFiles(fl));
+                    }
+
+                    //for (string. i = 0; string. i < 12; string. i++)
+
+                    //foreach (string line in System.IO.File.ReadLines($@"C:\\Users\\USER\\source\\repos\\Test2\\Test2\\Data\\Compartments\\S1.csv"))
+                    //     st.Add(line.Trim().Replace('.', ',').Split(';').Select(double.Parse).ToArray());
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("NOT READ");
+                    Console.WriteLine(e.Message);
+                }
+                return st;
+
+                List<Cargo> cargo = new List<Cargo>();
+                foreach (Double[] item in st)
+                {
+                    cargo.Add(new Cargo(item));
+                    foreach (var subitem in item)
+                        Console.Write(subitem + " ");
+                    Console.WriteLine();
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("NOT READ");
-                Console.WriteLine(e.Message);
-            }
-            List<Cargo> cargo = new List<Cargo>();
-            foreach (Double[] item in st)
-            {
-                cargo.Add(new Cargo(item));
-                foreach (var subitem in item)
-                    Console.Write(subitem + " ");
-                Console.WriteLine();
-            }
-            
             List<Double[]> st1 = new List<Double[]>();
             try
             {                
@@ -82,7 +103,9 @@ namespace Test2
             // попытался найти Vi для столбца Volume1, массива cargo. Выводит ошибку.
 
             //double V1i = Math.Abs((((cargo[pos2 + 1].Volume1 - cargo[pos2].Volume1))/ (cargo[pos2 + 1].Sounding- cargo[pos2].Sounding)) * (SoundingI - cargo[pos2].Sounding) + cargo[pos2].Volume1);
+
             Console.WriteLine(cargo[pos2].V[pos1+2]);
+
             double v00 = cargo[pos2].V[pos1 + 2];
             double v01 = cargo[pos2].V[pos1 + 2 + 1];
             double v10 = cargo[pos2 + 1].V[pos1 + 2];
@@ -121,9 +144,14 @@ namespace Test2
 
         private static double interpolate(double v00, double v01, double v10, double v11, double s, double d, double s0, double s1, double d0, double d1)
         {
-            double v1 = v00+(s-s0)*(v10-v00)/(s1-s0);
-            double v2 = v01+(s - s0) * (v11 - v01) / (s1 - s0);
-            double v3 = v1+(d-d0)*(v2-v1)/(d1-d0);
+            //double v1 = v00+(s-s0)*(v10-v00) / (s1-s0);
+            //double v2 = v01+(s - s0) * (v11 - v01) / (s1 - s0);
+            //double v3 = v1+(d-d0)*(v2-v1) / (d1-d0);
+
+            double v1 = v00 + (s - s0) * ((v10 - v00) / (s1 - s0));
+            double v2 = v01 + (s - s0) * ((v11 - v01) / (s1 - s0));
+            double v3 = v1 + (d - d0) * ((v2 - v1) / (d1 - d0));
+
             return v3;
         }
     }
